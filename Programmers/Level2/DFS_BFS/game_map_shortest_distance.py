aff860402c43513407902f36b04310a8df189578
Chan <33cks1423@naver.com>
2022-12-26 18:21:22 +0900

    Programmers Level 2

--- Programmers/Level2/DFS_BFS/game_map_shortest_distance.py
+++ Programmers/Level2/DFS_BFS/game_map_shortest_distance.py
@@ -1,10 +1,46 @@
+from collections import deque
+
+
+def direction(x,y,queue,maps):
+    dx = [1,0,-1,0]
+    dy = [0,1,0,-1]
+    for i in range(len(dx)):
+        nx = dx[i] + x
+        ny = dy[i] + y
+        if -1 < nx < len(maps[0]) and -1 < ny < len(maps):
+            if maps[ny][nx] == 1:
+                if ny == 0 and nx == 0:
+                    continue
+                # print(ny,nx)
+                maps[ny][nx] = maps[y][x] + 1
+                queue.append([nx,ny])
+            elif maps[ny][nx] > maps[y][x] + 1:
+                maps[ny][nx] = maps[y][x] + 1
+                queue.append([nx,ny])
+    return queue, maps
+
+
+
+def bfs(queue, maps):
+
+    while queue:
+        popleft = queue.popleft()
+        queue, maps = direction(popleft[0], popleft[1], queue, maps)
+    return maps
+
 def solution(maps):
-    answer = 0
+    queue = deque()
+    queue.append([0,0])
+    maps = bfs(queue, maps)
+    answer = maps[-1][-1]
+    # print(maps)
+    if answer == 1:
+        return -1
     return answer
 
 
 def main():
-    print(solution(4, 3,[[2, 2]]))
+    print(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]]))
 
 
 if __name__ == "__main__":
--- Programmers/Level2/Stack_Queue/a_truck_passing_through_a_bridge.py
+++ Programmers/Level2/Stack_Queue/a_truck_passing_through_a_bridge.py
@@ -1,25 +1,34 @@
-
-
+from collections import deque
 
 def solution(bridge_length, weight, truck_weights):
-    answer = 0
-    bridge = []
-    cnt = 0
-    while bridge or truck_weights:
-        truck = truck_weights.pop(0)
-        bridge.append([truck, 0])
-        # 다리 길이 확인
-        valid = True
-        if sum(bridge) > weight:
-            valid = False
-            truck_weights.insert(0, bridge.pop())
+    all_truck = len(truck_weights)
+    completed_pass_truck = 0
+    cross_pass_truck = deque()
+    timer_pass_truck = []
+    time_count = 0
+    while completed_pass_truck != all_truck:
+        time_count += 1
+        try:
+            for i in range(len(timer_pass_truck)):
+                timer_pass_truck[i] += 1
+            if timer_pass_truck[0] == bridge_length:
+                cross_pass_truck.popleft()
+                timer_pass_truck.pop(0)
+                completed_pass_truck += 1
+        except:
+            pass
 
-        if valid:
-            cnt += 1
+        try:
+            if sum(cross_pass_truck) + truck_weights[0] <= weight:
+                truck_weights_pop = truck_weights.pop(0)
+                cross_pass_truck.append(truck_weights_pop)
+                timer_pass_truck.append(0)
+        except:
+            continue
+
+    return time_count
 
 
-
-    return answer
 
 def main():
     print(solution(2, 10, [7,4,5,6]))
