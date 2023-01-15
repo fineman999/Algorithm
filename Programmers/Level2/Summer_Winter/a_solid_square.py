39d86f09c6ecdf5babc825df2941f8ef9bbd1269
Chan <33cks1423@naver.com>
2023-01-16 00:53:49 +0900

    Success: Programmers -> Level 2 -> Summer/Winter Coding(2019) -> 멀쩡한 사각형

--- Programmers/Level2/Summer_Winter/a_solid_square.py
+++ Programmers/Level2/Summer_Winter/a_solid_square.py
@@ -8,35 +8,33 @@ def solution(w,h):
         return w*h-w
     big_w = w
     big_h = h
-    #33
+
     lcm = divide(w,h)
-    w = w // lcm
-    h = h // lcm
-    cnt = 0
-    arr = [[0] * (w+1) for _ in range(h+1)]
-
-    for j in range(0, h+1):
-        for i in range(0, w//2+1):
-            if j*w > i*h:
-                arr[j][i] = 1
-            elif j*w < i*h:
-                arr[j][i] = -1
-            else:
-                arr[j][i] = 0
-
+    # w = w // lcm
+    # h = h // lcm
+    # print(w,h)
+    # cnt = 0
+    #
+    # arr = [[0] * (w+1) for _ in range(h+1)]
+    #
+    # for j in range(0, h+1):
+    #     for i in range(0, w+1):
+    #         if j*w > i*h:
+    #             arr[j][i] = 1
+    #         elif j*w < i*h:
+    #             arr[j][i] = -1
+    #         else:
+    #             arr[j][i] = 0
+    #
     # for j in range(0, h):
-    # for j in range(0, h):
-    #     if j*w > 1*h:
-    #         arr[j][1] = 1
-    #     elif (j+1)*w < 0:
-    #         arr[j+1][0] = -1
-    #     if arr[j][1] + arr[j+1][0] == 0:
-    #         cnt += 1
-    # cnt = cnt*2
-    return big_h*big_w - cnt*lcm
+    #     for i in range(1, (w+1)):
+    #         if arr[j][i] + arr[j+1][i-1] == 0:
+    #             cnt += 1
 
+    # return big_h*big_w - cnt*lcm
+    return big_w*big_h - (big_w+big_h-lcm)
 def main():
-    print(solution(8, 12))
+    print(solution(38, 12))
 
 
 if __name__ == "__main__":
