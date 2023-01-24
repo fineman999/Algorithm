613cad7852144969e494db150b00c603cb14efeb
Chan <33cks1423@naver.com>
2023-01-24 17:57:25 +0900

    Success: Programmers -> Level 3 -> 2019 카카오 개발자 겨울 인턴십 -> 불량 사용자(난이도: 중)

--- Programmers/Level3/kakao/bad_user.py
+++ Programmers/Level3/kakao/bad_user.py
@@ -1,31 +1,62 @@
-import itertools
+import copy
+result = set()
+def dfs(n, user_id, dictionary, visited):
+    global result
+    if n == visited.count(True):
+        check = []
+        for i in range(len(user_id)):
+            if visited[i]:
+                check.append(user_id[i])
+        result.add(tuple(check))
+        return
 
+    for i in range(len(user_id)):
+        if not visited[i]:
+            for key,value in dictionary.items():
+                if dictionary[key]>0 and check_banned_valid(user_id[i], key):
+                    new_dictionary = copy.deepcopy(dictionary)
+                    new_dictionary[key] -= 1
+                    new_visited = copy.deepcopy(visited)
+                    new_visited[i] = True
+                    visited[i] = True
+                    dfs(n, user_id, new_dictionary, new_visited)
+                    visited[i] = False
+    return
 
+def check_banned_valid(user, banned):
+    valid = False
+    if len(banned) == len(user):
+        valid = True
+        for i in range(len(banned)):
+            if banned[i] == "*":
+                continue
+            if banned[i] != user[i]:
+                valid = False
+                break
+    return valid
 def solution(user_id, banned_id):
-    arr = []
 
+    n = len(banned_id)
+    dictionary = dict()
+    for i in banned_id:
+        if i not in dictionary:
+            dictionary[i] = 1
+        else:
+            dictionary[i] += 1
+    for i in range(len(user_id)):
+        for key,value in dictionary.items():
+            if dictionary[key] > 0 and check_banned_valid(user_id[i],key):
+                visited = [False] * len(user_id)
+                new_dictionary = copy.deepcopy(dictionary)
+                new_dictionary[key] -= 1
+                visited[i] = True
+                dfs(n, user_id, new_dictionary, visited)
 
-    for banned in banned_id:
-        for user in user_id:
-            if len(banned) == len(user):
-                valid = True
-                for i in range(len(banned)):
-                    if banned[i] == "*":
-                        continue
-                    if banned[i] != user[i]:
-                        valid = False
-                        break
-                if valid:
-                    arr.append(user)
-    s = set(arr)
-    combinations = itertools.combinations(s, len(banned_id))
-
-    return s
-
+    return len(result)
 
 
 def main():
-    print(solution(["frodo", "fradi", "crodo", "abc123", "frodoc"],["*rodo", "*rodo", "******"]))
+    print(solution(	["frodo", "fradi", "crodo", "abc123", "frodoc"], ["fr*d*", "*rodo", "******", "******"]))
 
 
 if __name__ == "__main__":
