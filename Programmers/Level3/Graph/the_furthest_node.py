ddb09d30b6f1517fd81394474d0e9bb0bf6167ab
Chan <33cks1423@naver.com>
2023-01-29 23:58:50 +0900

    Success: Programmers -> Level 3 -> 그래프  -> 가장 먼 노드(난이도: 상): BFS 큐 이용, recursive와 큐를 동시에 사용하여 오류 발생, 큐를 통한 while문 작성 명심하기

--- Programmers/Level3/Graph/the_furthest_node.py
+++ Programmers/Level3/Graph/the_furthest_node.py
@@ -1,49 +1,39 @@
 from collections import deque
 visited = []
 
-def bfs(graph, start, queue):
+def bfs(graph, queue):
 
     global visited
     while queue:
-        queue_popleft = queue.popleft()
-        for ele in queue_popleft:
-            if visited[ele] == -1 or visited[ele] > 1 + visited[start]:
-                visited[ele] = 1 + visited[start]
+        now = queue.popleft()
+        for node in graph[now]:
+            if visited[node] == -1:
+                queue.append(node)
+                visited[node] = visited[now] + 1
 
-        for ele in queue_popleft:
-                set_ele = []
-                for i in range(len(graph[ele])):
-                    if visited[graph[ele][i]] == -1 or visited[graph[ele][i]] > visited[ele] + 1 or graph[ele][i] != start:
-                        visited[graph[ele][i]] = visited[ele] + 1
-                        set_ele.append(graph[ele][i])
-                if set_ele:
-                    queue.append(set_ele)
-                    bfs(graph, ele, queue)
 
 def solution(n, edge):
+
     global visited
+    # print(edge)
     visited = [-1]*(n+1)
     graph = [[] for _ in range(n+1)]
     for ele in edge:
         [node_1, node_2] = ele
-        # if node_1 > node_2:
-        #     node_1, node_2 = node_2, node_1
         graph[node_1].append(node_2)
         graph[node_2].append(node_1)
-    print(graph)
-    # setting
+
     queue = deque()
-    queue.append(graph[1])
-    visited[1] = 0
-    bfs(graph, 1, queue)
-
+    queue.append(1)
+    visited[1] = 1
+    bfs(graph, queue)
     max_visited = max(visited)
-
+    # print(visited)
     return visited.count(max_visited)
 
 
 def main():
-    print(solution(6,[[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]))
+    print(solution(7,[[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2],[5,7],[3,7]]))
 
 
 if __name__ == "__main__":
