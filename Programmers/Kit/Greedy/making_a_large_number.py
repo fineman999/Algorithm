828bd59c1bac8f68be23bf87c606e44d20d2eb09
Chan <33cks1423@naver.com>
2023-02-14 20:08:50 +0900

    [#35]: 큰 수 만들기

---
+++ Programmers/Kit/Greedy/making_a_large_number.py
@@ -0,0 +1,27 @@
+from collections import deque
+def solution(number, k):
+    number = list(map(int, number))
+    queue = deque(number)
+    stack = deque()
+
+    cnt = 0
+    while queue:
+        if cnt == k:
+            break
+        if not stack:
+            stack.append(queue.popleft())
+        elif stack[-1] < queue[0]:
+            stack.pop()
+            cnt += 1
+        else:
+            stack.append(queue.popleft())
+
+    return "".join(list(map(str, stack)) + list(map(str, queue)))
+
+
+def main():
+    print(solution("1231234", 3))
+
+
+if __name__ == "__main__":
+    main()
