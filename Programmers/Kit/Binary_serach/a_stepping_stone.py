46683f9e6126b2d16eb0ec5ead9769f7981229e2
Chan <33cks1423@naver.com>
2023-02-16 17:51:11 +0900

    [#49]: 징검다리

--- Programmers/Kit/Binary_serach/a_stepping_stone.py
+++ Programmers/Kit/Binary_serach/a_stepping_stone.py
@@ -1,6 +1,3 @@
-import math
-from collections import defaultdict
-
 answer = 0
 def binary_search(rocks, start, end, n):
 
@@ -8,12 +5,9 @@ def binary_search(rocks, start, end, n):
         return
     mid = (end + start) // 2
 
-    # print("mid",mid)
     check_rocks = 0
     check_distance = 0
     dist = []
-    # print(rocks,"rocks")
-    # print("mid",mid)
     for i in range(1, len(rocks)):
         check_distance += rocks[i] - rocks[i - 1]
         if check_distance >= mid:
@@ -21,27 +15,25 @@ def binary_search(rocks, start, end, n):
             check_distance = 0
         else:
             check_rocks += 1
+        if check_rocks > n:
+            break
     if check_distance > 0:
         dist.append(check_distance)
 
-    if check_rocks == n:
-        global answer
-        result = min(dist)
-        answer = max(answer, result)
     if check_rocks > n:
         binary_search(rocks, start, mid - 1, n)
     else:
+        global answer
+        result = min(dist)
+        answer = max(answer, result)
         binary_search(rocks, mid + 1, end, n)
+
 
 def solution(distance, rocks, n):
     rocks.sort()
     rocks.insert(0, 0)
     rocks.append(distance)
-    dist = []
-    for i in range(1, len(rocks)):
-        dist.append(rocks[i]-rocks[i-1])
-    global answer
-    answer = min(dist)
+
     end = distance
     binary_search(rocks, 0, end, n)
     return answer
