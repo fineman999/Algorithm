5f75ac7c08ed7a38cdbc06679354bd5ec254484f
Chan <33cks1423@naver.com>
2023-02-23 16:50:00 +0900

    [#74] 시소 짝꿍

--- Programmers/Level2/PracticeQuestion/siso.py
+++ Programmers/Level2/PracticeQuestion/siso.py
@@ -1,23 +1,34 @@
+from collections import defaultdict
+import itertools
 
 
 def valid_twin(weight1, weight2):
-    distance = [2,3,4]
+    distance = [2, 3, 4]
     for i in range(3):
         for j in range(3):
-            if weight1*distance[i] == weight2*distance[j]:
+            if weight1 * distance[i] == weight2 * distance[j]:
                 # print(weight1, weight2)
                 return True
     return False
 
 
+def change_twin(weights, diary):
+    distance = [2, 3, 4]
+    for i in range(len(weights)):
+        for dist in distance:
+            diary[dist * weights[i]].append(i)
+
 
 def solution(weights):
-    answer = 0
-    # visited = [False]*len(weights)
-    # for i in range(len(weights)):
-    #     dfs(weights, visited, weights[i])
-    for i in range(len(weights)):
-        for j in range(i+1, len(weights)):
-            if valid_twin(weights[i],weights[j]):
-                answer += 1
-    return answer
+    diary = defaultdict(list)
+
+    change_twin(weights, diary)
+
+    answer = set()
+    for key, value in diary.items():
+        if len(value) >= 2:
+
+            for i in itertools.combinations(value, 2):
+                answer.add(i)
+
+    return len(answer)
