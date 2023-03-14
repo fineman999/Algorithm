85349c365409e633a52b62c53b5c839a7acf8cc2
Chan <33cks1423@naver.com>
2023-03-14 20:43:54 +0900

    [#149] 1092번: 배

--- BaekJoon/Gold/Level5/ship.py
+++ BaekJoon/Gold/Level5/ship.py
@@ -1,30 +1,56 @@
 import sys
 import heapq
 from collections import deque
+# def solution(N, M, crane, boxes):
+#     crane.sort(reverse=True)
+#     boxes.sort(reverse=True)
+#     # print(crane)
+#     # print(boxes)
+#     min_box = min(boxes)
+#     answer = 0
+#     heap = deque(boxes)
+#     if crane[0] < heap[0]:
+#         return -1
+#     index = 0
+#     tmp = deque()
+#     while heap:
+#         while heap:
+#             check = heap.popleft()
+#             if check <= crane[index] and min_box <= crane[index]:
+#                 index += 1
+#             else:
+#                 tmp.append(check)
+#             if index == N:
+#                 break
+#         answer += 1
+#         while tmp:
+#             heap.appendleft(tmp.pop())
+#         index = 0
+#         # print(heap)
+#
+#     return answer
+
 def solution(N, M, crane, boxes):
     crane.sort(reverse=True)
     boxes.sort(reverse=True)
-    # print(crane)
-    # print(boxes)
-
+    print("crane", crane)
+    print("boxes", boxes)
     answer = 0
-    heap = deque(boxes)
-    if crane[0] < heap[0]:
+    if crane[0] < boxes[0]:
         return -1
     index = 0
-    tmp = deque()
-    while heap:
-        while heap:
-            check = heap.popleft()
-            if check <= crane[index]:
+    visited = [False]*M
+    count = 0
+    while count < M:
+        for i in range(M):
+            if not visited[i] and boxes[i] <= crane[index]:
                 index += 1
-            else:
-                tmp.append(check)
+                visited[i] = True
+                count += 1
             if index == N:
                 break
+
         answer += 1
-        while tmp:
-            heap.appendleft(tmp.pop())
         index = 0
 
     return answer
