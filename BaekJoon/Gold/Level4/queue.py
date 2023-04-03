ebf2f75842b32d617bed76bd0a174f4b84a30ab9
Chan <33cks1423@naver.com>
2023-04-03 21:04:46 +0900

    [#194] 7662번: 이중 우선순위 큐

--- BaekJoon/Gold/Level4/queue.py
+++ BaekJoon/Gold/Level4/queue.py
@@ -1,37 +1,43 @@
 import heapq
 import math
 import sys
-
+from collections import defaultdict
 
 def solution(arr, num):
+    # 최대힙과 최소힙 초기화
     heap_max = []
     heap_min = []
-    for ele in arr:
+    # index별로 최대힙과 최소힙에 들어간 숫자를 확인
+    diary = defaultdict(int)
+    # for문 실행
+    for i, ele in enumerate(arr):
+        # I일 경우 insert
         if ele[0] == 'I':
-            heapq.heappush(heap_max, -int(ele[1]))
-            heapq.heappush(heap_min, int(ele[1]))
+            heapq.heappush(heap_max, (-int(ele[1]), i))
+            heapq.heappush(heap_min, (int(ele[1]), i))
+            diary[i] += 1
         elif ele[0] =='D':
-            if int(ele[1]) == 1 and heap_max:
-                heapq.heappop(heap_max)
-                if not heap_max or heap_min[0] > -heap_max[0]:
-                    heap_min = []
-                    heap_max = []
-            elif heap_min:
-                heapq.heappop(heap_min)
-                if not heap_min or heap_min[0] > -heap_max[0]:
-                    heap_max = []
-                    heap_min = []
-    # print(heap_max, heap_min)
-    left = math.inf
-    if heap_min:
-        left = heapq.heappop(heap_min)
-    right = math.inf
-    if heap_max:
-        right = -heapq.heappop(heap_max)
-    if left == math.inf or right == math.inf:
-        return "EMPTY"
+            # D일 경우 1이면 ㅁ
+            if int(ele[1]) == 1:
+                while heap_max and heap_max[0][1] not in diary:
+                    heapq.heappop(heap_max)
+                if heap_max:
+                    del diary[heapq.heappop(heap_max)[1]]
+            else:
+                while heap_min and heap_min[0][1] not in diary:
+                    heapq.heappop(heap_min)
+                if heap_min:
+                    del diary[heapq.heappop(heap_min)[1]]
 
-    return f'{right} {left}'
+    # 들어가지 않은 값 확인
+    while heap_max and heap_max[0][1] not in diary:
+        heapq.heappop(heap_max)
+    while heap_min and heap_min[0][1] not in diary:
+        heapq.heappop(heap_min)
+
+    if not heap_min or not heap_max:
+        return "EMPTY"
+    return f'{-heap_max[0][0]} {heap_min[0][0]}'
 
 
 def main():
