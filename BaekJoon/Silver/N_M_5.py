c3d7d8cefb76c7f3a7807cb67317f33d3cca6775
Chan <33cks1423@naver.com>
2023-04-20 22:44:26 +0900

    [#210] 15654번: N과 M(5)

---
+++ BaekJoon/Silver/N_M_5.py
@@ -0,0 +1,30 @@
+import sys
+
+
+def dfs(N, M, sequence, cnt, temp):
+    if M == cnt:
+        print(*temp)
+        return
+    for i in range(N):
+        if sequence[i] not in temp:
+            temp.append(sequence[i])
+            dfs(N, M, sequence, cnt + 1, temp)
+            temp.remove(sequence[i])
+
+
+
+def solution(N, M, sequence):
+    sequence.sort()
+    cnt = 0
+    temp = []
+    dfs(N, M, sequence, cnt, temp)
+
+
+def main():
+    N, M = map(int, sys.stdin.readline().rstrip().split())
+    sequence = list(map(int, sys.stdin.readline().rstrip().split()))
+    solution(N, M, sequence)
+
+
+if __name__ == '__main__':
+    main()
