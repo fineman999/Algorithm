db302736c64b74ad3761583675b6f5a3d2702cf8
Chan <33cks1423@naver.com>
2023-04-22 22:44:54 +0900

    [#210] 15663번: N과 M(9)

--- BaekJoon/Silver/N_M_5.py
+++ BaekJoon/Silver/N_M_5.py
@@ -1,16 +1,22 @@
 import sys
 
-
-def dfs(N, M, sequence, cnt, temp):
+answer = set()
+def dfs(N, M, sequence, cnt, temp, visited):
     if M == cnt:
-        print(*temp)
+        tmp = ' '.join(map(str, temp))
+        if tmp not in answer:
+            print(tmp)
+            answer.add(tmp)
+
+        # print(*temp)
         return
     for i in range(N):
-        # if sequence[i] not in temp:
-        if not temp or temp[-1] <= sequence[i]:
+        if not visited[i]:
             temp.append(sequence[i])
-            dfs(N, M, sequence, cnt + 1, temp)
-            temp.remove(sequence[i])
+            visited[i] = True
+            dfs(N, M, sequence, cnt + 1, temp, visited)
+            temp.pop()
+            visited[i] = False
 
 
 
@@ -18,8 +24,8 @@ def solution(N, M, sequence):
     sequence.sort()
     cnt = 0
     temp = []
-    dfs(N, M, sequence, cnt, temp)
-
+    visited = [False]*N
+    dfs(N, M, sequence, cnt, temp, visited)
 
 def main():
     N, M = map(int, sys.stdin.readline().rstrip().split())
