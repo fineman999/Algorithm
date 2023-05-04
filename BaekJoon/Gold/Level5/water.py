ec1dcadc17315a85c0483e6ee0e864bc77f67121
Chan <33cks1423@naver.com>
2023-05-04 10:43:29 +0900

    [#220] 14719번: 빗물

---
+++ BaekJoon/Gold/Level5/water.py
@@ -0,0 +1,71 @@
+import sys
+from collections import deque
+dx = [1,0,-1]
+dy = [0,1,0]
+
+
+def check_valid(graph, i, j, W):
+    k = j - 1
+    left = False
+    while k >= 0:
+        if graph[i][k] == 1:
+            left = True
+            break
+        k -= 1
+
+    k = j + 1
+    right = False
+    while k <= W - 1:
+        if graph[i][k] == 1:
+            right = True
+            break
+        k += 1
+    if left and right:
+        return True
+
+    return False
+
+def bfs(q, graph, H, W):
+    cnt = 0
+    while q:
+        (x, y) = q.popleft()
+        cnt += 1
+        for i in range(3):
+            nx = dx[i] + x
+            ny = dy[i] + y
+            if -1 < ny < H and -1< nx < W and graph[ny][nx] == 0:
+                graph[ny][nx] = 1
+                q.append((nx, ny))
+    return cnt
+
+
+def solution(H, W, graph):
+    answer = 0
+    for i in range(H):
+        for j in range(W):
+            if graph[i][j] == 0:
+                if check_valid(graph, i, j, W):
+                    q = deque()
+                    q.append((j, i))
+                    graph[i][j] = 1
+                    answer += bfs(q, graph, H, W)
+    print(answer)
+
+
+def main():
+    H, W = inputs()
+    graph = [[0]*W for _ in range(H)]
+    arr = list(inputs())
+    for i in range(W):
+        for j in range(arr[i]):
+            graph[H-j-1][i] = 1
+
+    solution(H, W, graph)
+
+
+def inputs():
+    return map(int, sys.stdin.readline().rstrip().split())
+
+
+if __name__ == '__main__':
+    main()
