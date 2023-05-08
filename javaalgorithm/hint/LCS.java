ff1879e807bfda3f0108b0cfdcd6b6a4ff11f5be
Chan <33cks1423@naver.com>
2023-05-08 10:26:46 +0900

    [#123] 9251번: LCS - java

--- javaalgorithm/hint/LCS.java
+++ javaalgorithm/hint/LCS.java
@@ -1,7 +1,34 @@
 package hint;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class LCS {
-    public static void main(String[] args) {
-        System.out.println("heloo");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] A = init(br);
+        char[] B = init(br);
+
+        int[] dp = new int[A.length];
+
+        for (char c : B) {
+            int tmp = 0;
+            for (int j = 0; j < A.length; j++) {
+                if (tmp < dp[j]) tmp = dp[j];
+                else if (c == A[j]) dp[j] = Math.max(dp[j], tmp + 1);
+
+            }
+        }
+
+        int answer = Arrays.stream(dp).max().orElse(1);
+        System.out.println(answer);
     }
+
+    public static char[] init(BufferedReader br) throws IOException {
+        String A = br.readLine();
+        return A.toCharArray();
+    }
+
 }
