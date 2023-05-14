acc2a9cd43237293eab02cb8fef2b1154231a332
Chan <33cks1423@naver.com>
2023-05-14 19:41:38 +0900

    [#226] 2146번: 다리 만들기 - refactoring

--- BaekJoon/Gold/Level3/make_leg.py
+++ BaekJoon/Gold/Level3/make_leg.py
@@ -6,60 +6,64 @@ from collections import deque
 
 dx = [0,1,0,-1]
 dy = [1,0,-1,0]
+answer = math.inf
 
-def find_island(visited, graph, q, N):
-    legs = deque()
-    new_visited = copy.deepcopy(visited)
+
+def find_island(visited, graph, q, N, num):
+    global answer
+    while q:
+        x, y, cnt = q.popleft()
+
+        if cnt > answer:
+            return
+        if graph[y][x] != 0 and graph[y][x] != num:
+            answer = min(answer, cnt - 1)
+            return
+
+        for i in range(4):
+            nx = dx[i] + x
+            ny = dy[i] + y
+            if -1 < ny < N and -1 < nx < N and not visited[ny][nx] and graph[ny][nx] != num:
+                q.append((nx, ny, cnt + 1))
+                visited[ny][nx] = True
+    return math.inf
+
+
+def cnt_number(N, graph, q, num):
+
     while q:
         x, y = q.popleft()
         for i in range(4):
             nx = dx[i] + x
             ny = dy[i] + y
-            if -1 < ny < N and -1 < nx < N and visited[ny][nx] == math.inf:
-
-                if graph[ny][nx] == 1:
-                    q.append((nx, ny))
-                    visited[ny][nx] = -1
-                    new_visited[ny][nx] = -1
-                else:
-                    legs.append((nx, ny))
-                    new_visited[ny][nx] = 1
-
-    return legs, new_visited
+            if -1 < ny < N and -1 < nx < N and graph[ny][nx] == 1:
+                graph[ny][nx] = num
+                q.append((nx, ny))
 
 
-def find_min_dist(legs, visited, graph, N):
-    # for ele in visited:
-    #     print(ele)
-    # print()
-    while legs:
-        x, y = legs.popleft()
-
-        if graph[y][x] == 1:
-            return visited[y][x] - 1
-
-        for i in range(4):
-            nx = dx[i] + x
-            ny = dy[i] + y
-            if -1 < ny < N and -1 < nx < N and visited[y][x] + 1 < visited[ny][nx]:
-                visited[ny][nx] = visited[y][x] + 1
-                legs.append((nx, ny))
-
-    return math.inf
-
-def solution(N, graph):
-
-    visited = [[math.inf] * N for _ in range(N)]
-    answer = math.inf
+def make_number(N, graph):
+    num = 2
     for i in range(N):
         for j in range(N):
-            if graph[i][j] == 1 and visited[i][j] == math.inf:
+            if graph[i][j] == 1:
                 q = deque()
                 q.append((j, i))
-                visited[i][j] = -1
-                legs, new_visited = find_island(visited, graph, q, N)
-                answer = min(find_min_dist(legs, new_visited, graph, N), answer)
+                graph[i][j] = num
+                cnt_number(N, graph, q, num)
+                num += 1
 
+
+def solution(N, graph):
+    make_number(N, graph)
+
+    for i in range(N):
+        for j in range(N):
+            if graph[i][j] != 0:
+                visited = [[False] * N for _ in range(N)]
+                q = deque()
+                q.append((j, i, 0))
+                visited[i][j] = True
+                find_island(visited, graph, q, N, graph[i][j])
     print(answer)
 
 
