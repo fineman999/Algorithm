ff0da8b05f339324bea129bf6daba4e97e985058
Chan <33cks1423@naver.com>
2023-05-18 11:39:59 +0900

    [#148] 2212번: 센서 - 자바

--- javaalgorithm/hint/Sensor.java
+++ javaalgorithm/hint/Sensor.java
@@ -5,16 +5,17 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.StringTokenizer;
 
 public class Sensor {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int k = Integer.parseInt(br.readLine());
-        String[] strings = br.readLine().split(" ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int[] sensors = new int[n];
         for (int i = 0; i < n; i++) {
-            sensors[i] = Integer.parseInt(strings[i]);
+            sensors[i] = Integer.parseInt(st.nextToken());
         }
 
         Arrays.sort(sensors);
